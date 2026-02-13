de660fd91da848a0c3d80538874ab869b387ca52
Michael Haugeneder <michael.haugeneder@slf.ch>
2026-02-13 14:41:47 +0100

    adapt heat_histogram to instrument label vector

--- code/scripts/load_data.jl
+++ code/scripts/load_data.jl
@@ -58,7 +58,7 @@ evaldf4 = turb.readturbasnetcdf(joinpath(datapath, "3a_t2_csat_proc_cut.nc"), ev
 #evaldf6 = turb.readturbasnetcdf(joinpath(tower_outfile_stam, "tjkdf.nc"), evalstart, evalend)
 
 # Instrument metadata for labeling (corresponds to evaldf1-4 / fx1-4)
-srf_type = ["ice side", "ice side", "lead side", "lead side"]
+srf_type = ["floe side", "floe side", "lead side", "lead side"]
 heights = [0.9, 2.0, 1.1, 2.0]
 # Compose labels from instrument metadata
 instr_labels = ["$(srf_type[i]) $(heights[i])m" for i in 1:4]
