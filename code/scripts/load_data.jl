ca79183e7a7850942da611c64a699af31a5a3291
Michael Haugeneder <michael.haugeneder@slf.ch>
2026-02-26 10:30:01 +0100

    add Fourier spectra

--- code/scripts/load_data.jl
+++ code/scripts/load_data.jl
@@ -60,6 +60,7 @@ evaldf4 = turb.readturbasnetcdf(joinpath(datapath, "3c_t2_csat_proc_cut.nc"), ev
 # Instrument metadata for labeling (corresponds to evaldf1-4 / fx1-4)
 srf_type = ["lead", "lead", "floe", "floe"]
 heights = [1.1, 2.2, 1.1, 2.2]
+instr_type = ["IRG", "CSAT", "IRG", "CSAT"]
 # Compose labels from instrument metadata
 instr_labels = ["$(srf_type[i]) $(heights[i])m" for i in 1:4]
 
